61bd5391f57b280ee8f4fa86f1a5dd0e99d7344b
Johannes <jowettinger@gmail.com>
2015-11-26 20:21:48 +0100

    test files better organized

---
+++ test/smoketest.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEST_RESULT="$SCRIPT_DIR/../test-result"
+
+RSS_EDGE_TAG="$(cat tag)"
+RSS_MIDDLETIER_TAG="$(cat tag)"
+
+docker-compose up -d
+
+sleep 60
+
+docker-compose run --rm cassandra sh -c "cqlsh -e \"$(cat $SCRIPT_DIR/create-db.cql)\" cassandra 9042"
+
+if [[ 200 -eq $(curl --write-out %{http_code} --silent --output /dev/null http://localhost:9090/jsp/rss.jsp) ]]; then
+  echo "edge endpoint returned 200"
+else
+  echo "edge endpoint did not returned 200"
+  exit 1
+fi
+
+docker-compose kill
+
+echo "true" > $TEST_RESULT
